0b683b4be8b8a122ccbdf97744bc73652e2c9384
Jonathan1700 <jonatha06castro@gmail.com>
2026-05-12 12:38:33 -0500

    .

--- Bloque_0/ejercico1.py
+++ Bloque_0/ejercico1.py
@@ -1,10 +1,14 @@
-class libro:
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+class Libro:
     def __init__(self, titulo, autor, isbn):
         self.titulo = titulo
         self.autor = autor
         self.isbn = isbn
 
-class categoria:
+class Categoria:
     def __init__(self, nombre, genero):
         self.nombre = nombre
         self.genero = genero
@@ -26,3 +30,23 @@ class Autor:
     def __init__(self, nombre, libros_publicados):
         self.nombre = nombre
         self.libros_publicados = libros_publicados
+
+
+Cristina = Persona("Cristina", 20)
+print(f"Persona: {Cristina.nombre}, Edad: {Cristina.edad}")
+
+Elian = Autor("Elian", ["El temor de un Hombre Sabio", "El ensayo de la segera"])
+print(f"Autor: {Elian.nombre}, Libros Publicados: {Elian.libros_publicados}")
+
+Jose = Usuario("Jose", "987654321")
+print(f"Usuario: {Jose.nombre}, Cédula: {Jose.cedula}")
+
+print ("Clases Identificadas:")
+print ("- Libro")
+print ("- Categoría")
+print ("- Usuario")
+print ("- Préstamo")
+print ("- Autor")
+
+
+
